4ebda715e27ea8c13b6aa50becc3e12932e2da08
Harigovind V H <harigovind.vh@experionglobal.local>
2021-12-14 19:11:37 +0530

    BE created

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import Hapi from '@hapi/hapi';
+
+const start = async () => {
+    const server = Hapi.server( {
+        port: 8000,
+        host: 'localhost',
+    });
+
+    server.route({
+        method:'GET',
+        path: '/hello',
+        handler
+    })
+
+    await server.start();
+    console.log(`server is listening on ${server.info.uri}`);
+}
+
+process.on('unhandledRejection', err=> {
+    console.log(err);
+    process.exit(1);
+})
+
+start();
